80932bf393bf9ae23d61b0ff0690737950ba5368
iswa-git <ibrikisw@gmail.com>
2023-02-22 02:13:02 +0200

    build again

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
             <Tesbihbs text={ "لا إله إلا الله وحده لا شريك له، لهُ الملك، ولهُ الحمد، وهو على كل شيء قدير"  } />
             <Tesbihbs text={ "أستغفر الله الذى لا إله إلا هو الحيّ القيّوم وأتوب إليه"  } />
             <Tesbihbs text={ "سبحان الله وبحمده سبحان الله العظيم"  } />  
-            <Tesbihbs text={ " test "} />
+            <Tesbihbs text={ " another test "} />
         </div>
     </div>
     
